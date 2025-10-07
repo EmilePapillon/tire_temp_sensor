002e663aed6052ab022e3733d1f3e88934ce0174
Emile Papillon <epapillon@live.ca>
2025-10-07 17:27:38 -0400

    moved file to abide by convention

--- include/arduino_logger.hh
+++ include/arduino_logger.hh
@@ -3,8 +3,12 @@
 
 class ArduinoLogger : public Logger {
 public:
+    ArduinoLogger(Level level = Level::INFO) : Logger(level) {}
     void log(Level level, const char* message) override {
         const char* level_str = "";
+        if (log_level_ > level) {
+            return; // Skip messages below the current log level
+        }
         switch (level) {
             case Level::DEBUG: level_str = "DEBUG"; break;
             case Level::INFO:  level_str = "INFO";  break;
